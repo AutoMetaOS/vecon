57608684a8b7178e8a9ae8cba83eae58d7225fda
Manav <51614345+plutoniumm@users.noreply.github.com>
2022-07-12 16:54:39 +0530

    redice

--- README.md
+++ README.md
@@ -1,32 +1,6 @@
 # create-svelte
 
-A CLI for creating new [SvelteKit](https://kit.svelte.dev) projects. Just run...
-
-```bash
-npm init svelte
-```
-
-...and follow the prompts.
-
-## API
-
-You can also use `create-svelte` programmatically:
-
-```js
-import { create } from 'create-svelte';
-
-await create('my-new-app', {
-  name: 'my-new-app',
-  template: 'default', // or 'skeleton'
-  types: 'checkjs', // or 'typescript' or null;
-  prettier: false,
-  eslint: false,
-  playwright: false
-});
-```
-
-`checkjs` means your project will use TypeScript to typecheck JavaScript via [JSDoc comments](https://www.typescriptlang.org/docs/handbook/jsdoc-supported-types.html).
+Based on [create-svelte](https://github.com/sveltejs/kit/tree/master/packages/create-svelte)
 
 ## License
-
-[MIT](../../LICENSE).
+[MIT](./LICENSE)
--- bin.js
+++ bin.js
@@ -8,67 +8,65 @@ import { dist } from './utils.js';
 
 // prettier-ignore
 const disclaimer = `
-${bold(cyan('Welcome to SvelteKit!'))}
+${ bold( cyan( 'Welcome to SvelteKit!' ) ) }
 
-${bold(red('This is beta software; expect bugs and missing features.'))}
+${ bold( red( 'This is beta software; expect bugs and missing features.' ) ) }
 
-Problems? Open an issue on ${cyan('https://github.com/sveltejs/kit/issues')} if none exists already.
+Problems? Open an issue on ${ cyan( 'https://github.com/sveltejs/kit/issues' ) } if none exists already.
 `;
 
-const { version } = JSON.parse(fs.readFileSync(new URL('package.json', import.meta.url), 'utf-8'));
+const { version } = JSON.parse( fs.readFileSync( new URL( 'package.json', import.meta.url ), 'utf-8' ) );
 
-async function main() {
-	console.log(gray(`\ncreate-svelte version ${version}`));
-	console.log(disclaimer);
+async function main () {
+	console.log( gray( `\ncreate-svelte version ${ version }` ) );
+	console.log( disclaimer );
 
-	let cwd = process.argv[2] || '.';
+	let cwd = process.argv[ 2 ] || '.';
 
-	if (cwd === '.') {
-		const opts = await prompts([
+	if ( cwd === '.' ) {
+		const opts = await prompts( [
 			{
 				type: 'text',
 				name: 'dir',
 				message: 'Where should we create your project?\n  (leave blank to use current directory)'
 			}
-		]);
+		] );
 
-		if (opts.dir) {
+		if ( opts.dir )
 			cwd = opts.dir;
-		}
 	}
 
-	if (fs.existsSync(cwd)) {
-		if (fs.readdirSync(cwd).length > 0) {
-			const response = await prompts({
+	if ( fs.existsSync( cwd ) ) {
+		if ( fs.readdirSync( cwd ).length > 0 ) {
+			const response = await prompts( {
 				type: 'confirm',
 				name: 'value',
 				message: 'Directory not empty. Continue?',
 				initial: false
-			});
+			} );
 
-			if (!response.value) {
-				process.exit(1);
-			}
+			if ( !response.value )
+				process.exit( 1 );
 		}
 	}
 
-	const options = /** @type {import('./types/internal').Options} */ (
+	const options = (
 		await prompts(
 			[
 				{
 					type: 'select',
 					name: 'template',
 					message: 'Which Svelte app template?',
-					choices: fs.readdirSync(dist('templates')).map((dir) => {
-						const meta_file = dist(`templates/${dir}/meta.json`);
-						const { title, description } = JSON.parse(fs.readFileSync(meta_file, 'utf8'));
+					choices: fs.readdirSync( dist( 'templates' ) ).map( ( dir ) => {
+						const meta_file = dist( `templates/${ dir }/meta.json` );
+						const { title, description } = JSON.parse( fs.readFileSync( meta_file, 'utf8' ) );
 
 						return {
 							title,
 							description,
 							value: dir
 						};
-					})
+					} )
 				},
 				{
 					type: 'select',
@@ -114,60 +112,60 @@ async function main() {
 			],
 			{
 				onCancel: () => {
-					process.exit(1);
+					process.exit( 1 );
 				}
 			}
 		)
 	);
 
-	options.name = path.basename(path.resolve(cwd));
+	options.name = path.basename( path.resolve( cwd ) );
 
-	await create(cwd, options);
+	await create( cwd, options );
 
-	console.log(bold(green('\nYour project is ready!')));
+	console.log( bold( green( '\nYour project is ready!' ) ) );
 
-	if (options.types === 'typescript') {
-		console.log(bold('✔ Typescript'));
-		console.log('  Inside Svelte components, use <script lang="ts">');
-	} else if (options.types === 'checkjs') {
-		console.log(bold('✔ Type-checked JavaScript'));
-		console.log('  https://www.typescriptlang.org/tsconfig#checkJs');
+	if ( options.types === 'typescript' ) {
+		console.log( bold( '✔ Typescript' ) );
+		console.log( '  Inside Svelte components, use <script lang="ts">' );
+	} else if ( options.types === 'checkjs' ) {
+		console.log( bold( '✔ Type-checked JavaScript' ) );
+		console.log( '  https://www.typescriptlang.org/tsconfig#checkJs' );
 	}
 
-	if (options.eslint) {
-		console.log(bold('✔ ESLint'));
-		console.log(cyan('  https://github.com/sveltejs/eslint-plugin-svelte3'));
+	if ( options.eslint ) {
+		console.log( bold( '✔ ESLint' ) );
+		console.log( cyan( '  https://github.com/sveltejs/eslint-plugin-svelte3' ) );
 	}
 
-	if (options.prettier) {
-		console.log(bold('✔ Prettier'));
-		console.log(cyan('  https://prettier.io/docs/en/options.html'));
-		console.log(cyan('  https://github.com/sveltejs/prettier-plugin-svelte#options'));
+	if ( options.prettier ) {
+		console.log( bold( '✔ Prettier' ) );
+		console.log( cyan( '  https://prettier.io/docs/en/options.html' ) );
+		console.log( cyan( '  https://github.com/sveltejs/prettier-plugin-svelte#options' ) );
 	}
 
-	if (options.playwright) {
-		console.log(bold('✔ Playwright'));
-		console.log(cyan('  https://playwright.dev'));
+	if ( options.playwright ) {
+		console.log( bold( '✔ Playwright' ) );
+		console.log( cyan( '  https://playwright.dev' ) );
 	}
 
-	console.log('\nInstall community-maintained integrations:');
-	console.log(cyan('  https://github.com/svelte-add/svelte-adders'));
+	console.log( '\nInstall community-maintained integrations:' );
+	console.log( cyan( '  https://github.com/svelte-add/svelte-adders' ) );
 
-	console.log('\nNext steps:');
+	console.log( '\nNext steps:' );
 	let i = 1;
 
-	const relative = path.relative(process.cwd(), cwd);
-	if (relative !== '') {
-		console.log(`  ${i++}: ${bold(cyan(`cd ${relative}`))}`);
+	const relative = path.relative( process.cwd(), cwd );
+	if ( relative !== '' ) {
+		console.log( `  ${ i++ }: ${ bold( cyan( `cd ${ relative }` ) ) }` );
 	}
 
-	console.log(`  ${i++}: ${bold(cyan('npm install'))} (or pnpm install, etc)`);
+	console.log( `  ${ i++ }: ${ bold( cyan( 'npm install' ) ) } (or pnpm install, etc)` );
 	// prettier-ignore
-	console.log(`  ${i++}: ${bold(cyan('git init && git add -A && git commit -m "Initial commit"'))} (optional)`);
-	console.log(`  ${i++}: ${bold(cyan('npm run dev -- --open'))}`);
+	console.log( `  ${ i++ }: ${ bold( cyan( 'git init && git add -A && git commit -m "Initial commit"' ) ) } (optional)` );
+	console.log( `  ${ i++ }: ${ bold( cyan( 'npm run dev -- --open' ) ) }` );
 
-	console.log(`\nTo close the dev server, hit ${bold(cyan('Ctrl-C'))}`);
-	console.log(`\nStuck? Visit us at ${cyan('https://svelte.dev/chat')}\n`);
+	console.log( `\nTo close the dev server, hit ${ bold( cyan( 'Ctrl-C' ) ) }` );
+	console.log( `\nStuck? Visit us at ${ cyan( 'https://svelte.dev/chat' ) }\n` );
 }
 
 main();
